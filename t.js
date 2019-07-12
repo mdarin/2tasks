2a3f19acdc704945e7266aa265d246f021509320
darin-m <darin.m@tvzavr.ru>
2019-07-12 16:47:07 +0300

    реализована задача в соответствии с задачником

--- t.js
+++ t.js
@@ -36,86 +36,147 @@ for (e in res) {
 	console.log(res[e]);
 }
 
+// 2. You have to remove the parentheses and reverse the word inside.  
+//
+// foo(bar) => foorab 
+// (bar) => rab 
+// foo(bar)blim => foorabblim 
+// foo(foo(bar))blim => foobaroofblim
+let tests = [
+	"(bar)foo => rabfoo",
+ 	"foo(bar) => foorab", 
+ 	"(bar) => rab",
+ 	"foo(foo(bar))blim => foobaroofblim",
 
-// 2 You have to remove the parentheses and reverse the word inside.
-// foo(bar) => foorab
-// (bar) => rab
-// foo(bar)blim => foorabblim
-// foo(foo(bar))blim => fooraboofblim
-let s = ["foo(bar)", "(bar)", "foo(bar)blim", "foo(foo(bar))blim"];
-let processed = s.map(function (word) {
-	
-	let letters = [];
-	for (let i = 0; i < word.length; i++) {
-		letters.push(word.charAt(i));
-	}
+	"((bar)) => bar",
+ 	"foo(bar)blim => foorabblim",
 
-	let res = new Array();
-	let state = 'direct';
+	"foo(foo((bar)))blim => fooraboofblim",
+
+	"foo((baz)foo(bar(bar))) => foorabbaroofbaz",
+	"foo((baz)foo(bar(bar)))blim =>foobarraboofbazblim",
+	"(bar)foo((BAZ)FoO(baz))blim => rabfoobazOoFBAZblim",
+	"(bar)foo(((BAR)BAZ)FoO(baz(bar)))blim => rabfoorabbazOoFBAZRABblim",
+]
+
+tests.forEach(function (item) {
+	item = item.replace(/[ \t]+/g,'').split('=>');
+	let input = item[0];
+	let model = item[1];
+	let result = reverse(input);
+	console.log(input, ' => ', model, ' | ', result);	
+});
+
+function reverse(input) {
+
 	let acc = new Array();
-	let stack = 0;
-	let i = 0;
+	let result = new Array();
+	let state = 'direct';
+	let stack = 0; 
+	let stack0 = new Array();
 
-	// find and replace
-	while (i < letters.length) {
-		
-		// ctrl stack and skip parentheses
-		switch (letters[i]) {
-		case '(': 
+	for (let i = 0; i < input.length; i++) {
+
+		let c = input.charAt(i);
+	//	console.log('c: ', c);
+
+		switch (c) {
+		case '(':
+
+
+			if (acc.length) {
+				if (stack) {
+	//				console.log('[(]acc: ', acc.join(''));
+	//				console.log('[(]stack before: ', stack0);
+					stack0.push(acc.join(''));
+	//				console.log('[(]stack after: ', stack0);
+				} else {
+	//				console.log('[(]acc: ', acc.join(''));
+	//				console.log('[(]result before: ', result);
+					result.push(acc.join(''));
+	//				console.log('[(]result after: ', result);
+				}
+			}
+
 			stack++;
-			i++;
-			continue;
-			break;
-		case ')': 
-			stack--;
-			i++;
-			continue;
-			break;
-		}
-	
-		//bidirectinal processing
-		if (!stack) {
+//			console.log('[(]level: ', stack);
+
 			switch (state) {
-			case 'direct':
-				acc.push(letters[i]);
+			case 'reverse':
+				state = 'direct';	
 				break;
-			case 'reverse': 
-				res = res.concat(acc.reverse());
-				acc = new Array();
-				acc.push(letters[i]);
+			default:
+				state = 'reverse'
+				break;
+			}
+
+			acc = new Array();
+
+			break;
+		case ')':
+
+			stack--;
+//			console.log('[)]level: ', stack);
+			
+			if (acc.length) {
+				if (stack) {
+	//				console.log('[)]acc: ', acc.join(''));
+	//				console.log('[)]stack before: ', stack0);
+					stack0.push(acc.join(''));
+	//				console.log('[)]stack after: ', stack0);
+				} else {
+	//				console.log('[)]acc: ', acc.join(''));
+	//				console.log('[)]result before: ', result);
+					result.push(acc.join(''));
+	//				console.log('[)]result after: ', result);
+				}
+			}
+
+
+			switch (state) {
+			case 'reverse':
 				state = 'direct';
 				break;
-			}
-		} else {
-			switch (state) {
-			case 'direct':
-				res = res.concat(acc);
-				acc = new Array();
-				acc.push(letters[i]);
+			default:
 				state = 'reverse';
 				break;
-			case 'reverse': 
-				acc.push(letters[i]);
+			}
+
+			acc = new Array();
+
+			break;
+		default:
+			switch (state) {
+			case 'reverse':
+				acc.unshift(c);
+				break;
+			default:
+				acc.push(c);
 				break;
 			}
-		} 
+			break;
+		} // eof switch
 
-		i++;
-	}	
+	} // eof for
 
-	switch (state) {
-	case 'direct':
-		return res.concat(acc).join('');
-		break;
-	case 'reverse':
-		return res.concat(acc.reverse()).join('');
-		break;
+//	console.log('stack: ', stack0);
+
+	if (acc.length) {
+		result.push(acc.join(''));
 	}
 
-});
+//	console.log('result: ', result);
 
-// show result
-processed.forEach(function (e) { 
-	console.log(e);
-});
+	if (result.length > 1) {
+		let tail = result.pop() || '';
+		result = result.join('') + stack0.reverse().join('') + tail;
+	} else {
+		result = result.join('') + stack0.reverse().join('');
+	}
+
+//	console.log('==> ',result); 
+
+	return result;
+
+}
 
